5582c3af48b4f041dc32a7c057b7111ce6ecee8a
Sykie Chen <chen17@coder17.com>
2019-03-30 06:51:43 +0800

    fixed PID format

--- launcher.cmd
+++ launcher.cmd
@@ -1,3 +1,3 @@
 for /f "tokens=*" %%a IN ('where python') do set py=%%a
 for /f "tokens=3" %%a in ('wmic process call create "%py% monitor.py > %~dp0\1.txt"^,"%~dp0"^|find "ProcessId"') do set pid=%%a
-echo %pid% > pid.txt
+echo %pid:~0,-1% > pid.txt
